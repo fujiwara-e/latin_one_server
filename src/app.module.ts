1cc4f3419a3b1219948393b085cce206c4e018e2
hosokawa-kenshin <hosokawa2023@s.okayama-u.ac.jp>
2024-10-24 17:48:20 +0900

    Implement file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { NotificationsController } from './notifications/notifications.controlle
 import { InboxController } from './inbox/inbox.controller';
 import { NotificationsService } from './notifications/notifications.service';
 import { InboxService } from './inbox/inbox.service';
+import { ExcelService } from './excel/excel.service';
+import { ExcelController } from './excel/excel.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, NotificationsController, InboxController],
-  providers: [NotificationsService, InboxService],
+  controllers: [AppController, NotificationsController, InboxController, ExcelController],
+  providers: [NotificationsService, InboxService, ExcelService],
 })
 export class AppModule {}
---
+++ src/excel/excel.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+
+@Controller('excel')
+export class ExcelController {
+ @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, cb) => {
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+        cb(null, `${randomName}${extname(file.originalname)}`);
+      },
+    }),
+  }))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return {
+      message: 'File uploaded successfully',
+      filename: file.filename,
+    };
+  }
+}
